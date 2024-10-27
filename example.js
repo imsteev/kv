a02530f86f07df6947fa1811e66e55250e9d779a
Stephen Chung <spchung95@gmail.com>
2024-10-27 11:42:36 -0400

    memtable

--- example.js
+++ example.js
@@ -3,16 +3,13 @@ import { LSMTree } from "./lsm/index.js";
 const lsmTree = new LSMTree({
   dataFolder: "./data",
   levelPrefix: "kvdb",
-  flushIntervalMs: 100,
+  flushIntervalMs: 1000,
+  memtableMaxItems: 25,
 });
 
 for (let i = 0; i < 10000; i++) {
   console.log(i);
   await new Promise((resolve) => setTimeout(resolve, 50));
   const val = Math.trunc(Math.random() * 1_000);
-  lsmTree.put(i, val);
+  lsmTree.put(val, val);
 }
-
-// how to od a sleep?
-
-// await Promise.resolve();
--- lsm/index.js
+++ lsm/index.js
@@ -4,21 +4,40 @@ import { SortedStringTable } from "./sstable.js";
 import path from "path";
 
 export class LSMTree {
-  constructor({ initialState, dataFolder, levelPrefix, flushIntervalMs }) {
+  constructor({
+    initialState,
+    dataFolder,
+    levelPrefix,
+    flushIntervalMs,
+
+    // note: this is more of a "soft" max. there are some concurrency things to iron out to truly respect this number.
+    memtableMaxItems = 10_000,
+  }) {
+    // Every LSMTree is associated with a folder on disk. persistent data are
+    // store in "levels", which are suffixed with a number to indicate age.
+    // For example if dataFolder="./data" and levelPrefix="kvdb", your first level
+    // would be "./data/kvdb.0".
     this.dataFolder = dataFolder;
     this.levelPrefix = levelPrefix;
 
+    // active, in-memory dataset
     this.memtable = initialState || {};
-    this.flushMemtable = {};
+    this.memtableMaxItems = memtableMaxItems;
 
+    // periodically the LSMTree will flush data if the memtable is too large.
+    this.flushMemtable = {};
     this.isFlushing = false;
 
-    setInterval(async () => {
-      await this.flush();
+    setInterval(() => {
+      this.flush();
     }, flushIntervalMs);
   }
 
-  // looks in-memory first, if not found will look at sstables on disk.
+  /**
+   * looks in-memory first, if not found will look at sstables on disk.
+   * @param {string} key
+   * @returns {string | null}
+   */
   async get(key) {
     if (key in this.memtable) {
       return this.memtable[key];
@@ -34,21 +53,25 @@ export class LSMTree {
     return val;
   }
 
-  // todo: make this more robust.
+  /**
+   *
+   * @param {string} key
+   * @param {string} value
+   */
   async put(key, value) {
-    this.memtable[`${key}`] = value;
+    this.memtable[key] = value;
   }
 
   /**
    * load will load an SSTable from disk, if it exists. otherwise returns an
    * empty tree.
-   * @param {*} folder
-   * @param {*} prefix
+   * @param {string} folder
+   * @param {string} prefix
    */
-  async load() {
+  async load(level = 0) {
     let f;
     try {
-      f = await fs.open(this.levelPath(0));
+      f = await fs.open(this.levelPath(level));
     } catch {
       console.log("level file doesn't exist, nothing to load.");
       return;
@@ -66,26 +89,35 @@ export class LSMTree {
     }
   }
 
-  // flush flushes memtable to disk. newer files have lower numbers (this makes
-  // it easier on LSMTree code since you don't have to track how many files there
-  // are).
+  /**
+   * flushes memtable to disk. newer files have lower numbers (this makes
+   * it easier on LSMTree code since you don't have to track how many files there
+   * are).
+   * @param {number} level
+   * @returns
+   */
   async flush(level = 0) {
-    console.log({
-      isFlushing: this.isFlushing,
-      length: Object.keys(this.memtable).length,
-    });
-    if (this.isFlushing || Object.keys(this.memtable).length < 50) {
+    if (
+      this.isFlushing ||
+      Object.keys(this.memtable).length < this.memtableMaxItems
+    ) {
       return;
     }
     this.isFlushing = true;
+
+    // make space for a new level file.
+    await this.incrementLevels(level);
+
+    // swap!
     const tmp = this.memtable;
     this.memtable = {};
     this.flushMemtable = tmp;
+
+    // todo: probably use a better data structure for keeping keys sorted.
     const keyVals = Object.entries(this.flushMemtable);
     keyVals.sort((a, b) =>
       a[0].localeCompare(b[0], undefined, { numeric: true })
     );
-    console.log("flushing!");
     try {
       await fs.appendFile(
         this.levelPath(level),
@@ -94,18 +126,27 @@ export class LSMTree {
         }),
         { encoding: "utf-8" }
       );
+    } catch (err) {
+      console.log(`failed to flush data: ${err}`);
     } finally {
+      // note: if error happens it's possible for data loss. eek!
       this.isFlushing = false;
       this.flushMemtable = {};
     }
   }
 
-  async incrementLevels() {
-    let i = 100;
-    for (let i = 100; i >= 0; i--) {
+  /**
+   * incrementLevels renames level files so that they make space for a new file
+   * to be inserted at @baseLevel.
+   * @param {number} baseLevel
+   */
+  async incrementLevels(baseLevel) {
+    for (let i = 100; i >= baseLevel; i--) {
       try {
         await fs.rename(this.levelPath(i), this.levelPath(i + 1));
-      } catch {}
+      } catch {
+        console.log(`failed to rename level ${i}. skipping`);
+      }
     }
   }
 
